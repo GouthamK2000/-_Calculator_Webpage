9e35f585f910c0f44b08ae052c2058d733a5ffc6
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-01-21 18:11:47 +0530

    Entire work done

--- caljs.js
+++ caljs.js
@@ -1,7 +1,7 @@
 var numField1 = document.getElementById('numField1');
 var numField2 = document.getElementById('numField2');
 var ResultField=document.getElementById('X is What % of Y');
-form.addEventListener('submit',function(){
+form.addEventListener('submit',function(event){
                       
 if(!numField1.value ||!numField2.value){
 alert("Please enter both the values");
@@ -9,7 +9,12 @@ alert("Please enter both the values");
 }else{
     var x=parseFloat(numField1.value);
     var y=parseFLoat(numField2.value);
- alert(x+y);
+  var result =(x/y);
+    var percent= result*100;
+    
+    ResultField.innerText="Answer: "+percent+"%";
+    event.preventDefault();
 }
 });
 
+ 
