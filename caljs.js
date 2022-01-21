3e061f1d4bca4752e0b519d10494a85745bc3b3d
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-01-21 15:27:46 +0530

    added some more lines of java script

--- caljs.js
+++ caljs.js
@@ -1,4 +1,15 @@
 var numField1 = document.getElementById('numField1');
 var numField2 = document.getElementById('numField2');
-var ResultField=document.getElementById('ResultField');
+var ResultField=document.getElementById('X is What % of Y');
+form.addEventListener('submit',function(){
+                      
+if(!numField1.value ||!numField2.value){
+alert("Please enter both the values");
+
+}else{
+    var x=parseFloat(numField1.value);
+    var y=parseFLoat(numField2.value);
+ alert(x+y);
+}
+});
 
